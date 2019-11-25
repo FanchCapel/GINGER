d9239bc9c6cf18d4c5593431c22b7e31a448bc22
Sinan Ucak <ucak.sinan@gmail.com>
2019-11-25 17:24:48 +0100

    message associations and validations

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,5 +1,13 @@
 class Message < ApplicationRecord
+
+  # Associations
   belongs_to :user
   belongs_to :message_type
   belongs_to :experience
+
+  # Validations
+  validates :user, presence: true
+  validates :content, presence: true
+  validates :message_type, presence: true
+  validates :experience, presence: true
 end
