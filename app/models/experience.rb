edd62ec64d7491d3a8a1c51835e958111242cb21
Nathan Tempels <nathan.tempels@gmail.com>
2019-11-25 17:04:23 +0100

    added validations and relations

--- app/models/experience.rb
+++ app/models/experience.rb
@@ -1,3 +1,12 @@
 class Experience < ApplicationRecord
+  validates :user_id, presence: true
+  validates :budget, presence: true
+  validates :city, presence: true
+  validates :date, presence: true
+  validates :time_slot, presence: true
+
   belongs_to :user
+
+  has_many :experience_slices
+  has_many :messages
 end
