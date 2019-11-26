f7d31ac5ea00e52104b10f433e654abb6f76fe01
Sinan Ucak <ucak.sinan@gmail.com>
2019-11-26 17:07:29 +0100

    experience designer first version

--- app/controllers/experience_slices_controller.rb
+++ app/controllers/experience_slices_controller.rb
@@ -2,13 +2,38 @@ class ExperienceSlicesController < ApplicationController
 
   skip_before_action :authenticate_user!, only: [:new, :create]
 
-
   def new
-
+    @activities = Activity.all
+    @experience = Experience.find(params[:experience_id])
+    @experience_slice = ExperienceSlice.new
   end
 
   def create
+    @experience = Experience.find(params[:experience_id])
 
+    experience_slice_1 = ExperienceSlice.new(
+      activity_id: params[:experience_slice][:activity_1],
+      experience: @experience,
+      order: 1
+    )
+    experience_slice_1.save
+    experience_slice_2 = ExperienceSlice.new(
+      activity_id: params[:experience_slice][:activity_2],
+      experience: @experience,
+      order: 2
+    )
+    experience_slice_2.save
+    experience_slice_3 = ExperienceSlice.new(
+      activity_id: params[:experience_slice][:activity_3],
+      experience: @experience,
+      order: 3
+    )
+    experience_slice_3.save
+
+    @experience.prepared_at = Time.now
+    @experience.save
+
+    redirect_to experiences_path
   end
 
   private
--- app/javascript/plugins/init_sortable.js
+++ app/javascript/plugins/init_sortable.js
@@ -1,7 +1,7 @@
 import Sortable from 'sortablejs';
 
 const initSortable = () => {
-  const list = document.querySelector('#sortable');
+  const list = document.querySelector('#sortable-activities');
   Sortable.create(list);
 };
 
