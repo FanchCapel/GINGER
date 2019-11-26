af0f3c843f9fd0df755c82ee95c9e702d7991342
Sinan Ucak <ucak.sinan@gmail.com>
2019-11-26 17:16:18 +0100

    sortable removed

--- app/javascript/plugins/init_sortable.js
+++
@@ -1,8 +0,0 @@
-import Sortable from 'sortablejs';
-
-const initSortable = () => {
-  const list = document.querySelector('#sortable-activities');
-  Sortable.create(list);
-};
-
-export { initSortable };
