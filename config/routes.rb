d1cbbeaa2dbaa9d8eb6f2ca37811437e485e6cb0
Nathan Tempels <nathan.tempels@gmail.com>
2019-11-27 13:30:25 +0100

    in process

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: { registrations: "registrations" }
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :experiences, only: [:index, :new, :create, :edit, :update, :show] do
