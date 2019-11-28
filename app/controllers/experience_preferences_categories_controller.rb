c49706cb82a3dde6d6203d1c4df20cf51a066e8f
Sinan Ucak <ucak.sinan@gmail.com>
2019-11-28 17:40:39 +0100

    choose experience preferences

--- app/controllers/experience_preferences_categories_controller.rb
+++ app/controllers/experience_preferences_categories_controller.rb
@@ -4,17 +4,26 @@ class ExperiencePreferencesCategoriesController < ApplicationController
   before_action :set_experience
 
   def new
-
+    @categories = Category.all
+    @experience_preferences_category = ExperiencePreferencesCategory.new
   end
 
   def create
+    params[:experience_preferences_category][:category].each do |category_id|
+      next if category_id.empty?
 
+      ExperiencePreferencesCategory.create(
+        experience: @experience,
+        category_id: category_id
+      )
+    end
+    redirect_to root_path
   end
 
   private
 
-  def experience_slice_params
-    params.require(:experience_slice).permit(:experience, :category)
+  def experience_preference_category_params
+    params.require(:experience_preference_category).permit(:experience, :category)
   end
 
   def set_experience
