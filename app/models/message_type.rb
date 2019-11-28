218d230ccc14b30c90f66f05d0292c86fc45f5e4
Francois Capel <fanch.capel@gmail.com>
2019-11-28 14:46:12 +0100

    validation change

--- app/models/message_type.rb
+++ app/models/message_type.rb
@@ -1,9 +1,8 @@
 class MessageType < ApplicationRecord
-
+  TYPE = ["Day before", "Noon", "15", "Start", "End of exp 1", "End of exp 2", "End"]
   # Associations
   has_many :message_types
 
   # Validations
-  validates :type, presence: true, collection: ["Day before", "Noon", "15", "Start", "End of exp 1", "End of exp 2", "End"]
-
+  validates :type, presence: true, inclusion: { in: TYPE }
 end
