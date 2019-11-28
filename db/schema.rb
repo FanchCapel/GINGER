5882c3b21ddb81392eb70955c0349e42aba5ff95
Sinan Ucak <ucak.sinan@gmail.com>
2019-11-28 16:32:54 +0100

    preference_level added to experience

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_28_150118) do
+ActiveRecord::Schema.define(version: 2019_11_28_152921) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -67,6 +67,7 @@ ActiveRecord::Schema.define(version: 2019_11_28_150118) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "checkout_session_id"
+    t.integer "preference_level"
     t.index ["user_id"], name: "index_experiences_on_user_id"
   end
 
