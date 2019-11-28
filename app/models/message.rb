2ce475ae341a3da72edd6b37c5334ad946df2b63
Francois Capel <fanch.capel@gmail.com>
2019-11-28 14:23:12 +0100

    validations of tables changed

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,13 +1,11 @@
 class Message < ApplicationRecord
 
   # Associations
-  belongs_to :user
   belongs_to :message_type
   belongs_to :experience
 
   # Validations
   validates :user, presence: true
-  validates :content, presence: true
   validates :message_type, presence: true
   validates :experience, presence: true
 
