f6e57f2d4f7bed33d1b8d1a69c898c84119b0905
Francois Capel <fanch.capel@gmail.com>
2019-11-29 16:27:17 +0100

    envoi de message à tester

--- app/jobs/message_job.rb
+++ app/jobs/message_job.rb
@@ -1,13 +1,16 @@
 class MessageJob < ApplicationJob
   queue_as :default
-  def perform
+
+  def perform(message_id)
+    message = Message.find(message_id)
+    user = message.experience.user
     user.primary_number[0] = ''
     user.secondary_number[0] = ''
     client = Nexmo::Client.new(api_key: ENV['NEXMO_API_KEY'], api_secret: ENV['NEXMO_API_SECRET'])
     client.sms.send(
       from: "Votre Majordome Ginger",
       to: "41#{user.primary_number}",
-      text: "Confirmation de votre expérience GINGER"
+      text: "#{message_type.content}"
       )
   end
 end
