ac508ddb4506afc35b64b23f06f39833479de363
Sinan Ucak <ucak.sinan@gmail.com>
2019-12-05 17:30:15 +0100

    spiciness error corrected

--- app/controllers/experience_preferences_categories_controller.rb
+++ app/controllers/experience_preferences_categories_controller.rb
@@ -9,14 +9,16 @@ class ExperiencePreferencesCategoriesController < ApplicationController
   end
 
   def create
-    params[:experience_preferences_category][:category].each do |category_id|
-      next if category_id.empty?
+    unless params[:experience_preferences_category].nil?
+      params[:experience_preferences_category][:category].each do |category_id|
+        next if category_id.empty?
 
-      ExperiencePreferencesCategory.create(
-        experience: @experience,
-        category_id: category_id
-      )
-      # @experience.update(preference_level: params[:slider])
+        ExperiencePreferencesCategory.create(
+          experience: @experience,
+          category_id: category_id
+        )
+        # @experience.update(preference_level: params[:slider])
+      end
     end
     redirect_to new_experience_payment_path(@experience)
   end
